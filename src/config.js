1557bf79d9bef3348af6742810679ec752d1276a
nacho <jose-imhoff@hotmail.com>
2023-08-19 17:21:34 -0300

    cambios desafio final 16

--- src/config.js
+++ src/config.js
@@ -15,7 +15,7 @@ const config ={
     sessionSecret: secret,
     clientID:process.env.CLIENT_ID,
     clientSecret:process.env.CLIENT_SECRET,
-    callbackUrl:process.env.CALLBACK_URL,
+    callbackUrl:"https://desafiofinal-lpv4-dev.fl0.io/api/sessions/githubcallback",
     mode:process.env.DEVELOPMENT_MODE,
     jwtSecret:"auto4555",
     cookieName:process.env.COOKIE_NAME,
