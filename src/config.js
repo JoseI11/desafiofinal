f8e5fe027255c8c7f8a0b57165773e0d52d7b9e0
nacho <jose-imhoff@hotmail.com>
2023-08-18 21:11:42 -0300

    Revert "Cambios desafio final 3"
    This reverts commit b195b0300e474d0500e630ec817d290c859b5bcb.

--- src/config.js
+++ src/config.js
@@ -11,24 +11,18 @@ dotenv.config({
 const secret=process.env.SESSION_SECRET
 const connectiondatabase=process.env.DB_URL
 const config ={
-    dbUrl: "mongodb+srv://Jose11:0oUuqj0WonjsrAhP@codercluster.fwoec0u.mongodb.net/ecommerce?retryWrites=true&w=majority",
-    sessionSecret: "auto45",
-    clientID:"Iv1.b6e8eed3962c6879",
-    clientSecret:"c4cedaacb19a6174b7202900f50e218e94560055",
-    callbackUrl:"http://localhost:8080/api/sessions/githubcallback",
-    mode:"false",
-    jwtSecret:"auto4555",
-    cookieName:"jwtCookie",
-    loggermode:"DEVELOPMENT",
-    adminEmail:"adminCoder@coder.com",
-    adminPass:"adminCod3r123",
-    stripeSecret:"sk_test_51Na5WnB3q7njnQ2YsNazkmj1mGTnsudXtsUTiItZnAT7t3T2Ub1Jyh9z7NaGNYbuRlvqNd2uIBEwqJwGSUafUxFz00A7zcqWz4",
-    service:"gmail",
-    port:"587",
-    user:"ignacio8686@gmail.com",
-    password:"wulmvtihpeizvxlb",
-    accountsid:"AC72cade47f6b8b2a1dcbf42835fe37fc1",
-    authtoken:"f67daa9f286f5945d20f712b054a7ff2",
-    phonenumber:"+13203789427"
+    dbUrl: connectiondatabase,
+    sessionSecret: secret,
+    clientID:process.env.CLIENT_ID,
+    clientSecret:process.env.CLIENT_SECRET,
+    callbackUrl:process.env.CALLBACK_URL,
+    mode:process.env.DEVELOPMENT_MODE,
+    jwtSecret: process.env.JWT_SECRET,
+    cookieName:process.env.COOKIE_NAME,
+    loggermode:process.env.LOGGER,
+    adminEmail:process.env.ADMIN_EMAIL,
+    adminPass:process.env.ADMIN_PASSWORD,
+    stripeSecret:process.env.STRIPE_SECRET
+    
 }
 export default config
